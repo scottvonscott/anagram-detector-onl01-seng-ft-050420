79856d12d2631ca1fc63a420213ec3432fe34025
Scott  Keplinger <skeplin17@gmail.com>
2020-05-16 18:06:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize (word)
+    @word = word
+  end
+
+  def match (word_array)
+    matching_words = []
+    index_word = Array.new(@word.split("")).sort
+       word_array.collect do |words|
+       split_array_word = Array.new(words.split("")).sort
+       if index_word == split_array_word
+          matching_words << words
+     end
+    end
+    matching_words
+  end
+
+
+  end
