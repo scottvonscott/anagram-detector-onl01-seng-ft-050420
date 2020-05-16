33718a275eb68642c280299db051864c60e1a423
Scott  Keplinger <skeplin17@gmail.com>
2020-05-16 17:56:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,6 +11,10 @@ class Anagram
     index_word = Array.new(@word.split("")).sort
     word_array.each do |words|
       split_array_word = Array.new(words.split("")).sort
+      index_word == split_array_word
+      binding.binding.pry
+    end
+  end
 
 
   end
