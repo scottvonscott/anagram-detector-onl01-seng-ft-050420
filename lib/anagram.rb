b5d519ecc4391cdb9dd45836b01d6f158eff7306
Scott  Keplinger <skeplin17@gmail.com>
2020-05-16 17:56:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,9 @@ class Anagram
   end
 
   def match (word_array)
+    index_word = Array.new(@word.split("")).sort
     word_array.each do |words|
-      split_word = Array.new(words.split(""))
+      split_array_word = Array.new(words.split("")).sort
 
 
   end
